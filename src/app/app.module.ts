d60bc400354bb5a72c4a3834a767bedccfc0e122
Gaurang Dadheech <gaurangdadheech@gmail.com>
2018-01-15 19:17:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,37 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import{BluetoothSerial} from '@ionic-native/bluetooth-serial'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+   
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+  
+    IonicModule.forRoot(MyApp),
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothSerial,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+   
+    
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,131 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { AlertController, LoadingController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  output:any;
+  message:String;
+  responseTxt:any;
+  unpairedDevices: any;
+  pairedDevices: any;
+  statusMessage: string;
+  gettingDevices: Boolean;
+  constructor(public loadCtrl:LoadingController,private bluetoothSerial: BluetoothSerial, private alertCtrl: AlertController, public navCtrl:NavController,private ngZone: NgZone) {
+    bluetoothSerial.enable();
 
-  constructor(public navCtrl: NavController) {
+    
+  }
+  
+  startScanning() {
+    this.pairedDevices = null;
+    this.unpairedDevices = null;
+    this.gettingDevices = true;
+    this.bluetoothSerial.discoverUnpaired().then((success) => {
+      this.unpairedDevices = success;
+      this.gettingDevices = false;
+      success.forEach(element => {
+        
+      });
+    },
+      (err) => {
+        console.log(err);
+      })
+
+    this.bluetoothSerial.list().then((success) => {
+      this.pairedDevices = success;
+    },
+      (err) => {
+
+      })
+  }
+  success = (data) => alert(data);
+  fail = (error) => alert(error);
+
+  selectDevice(address: any) {
+
+    let alert = this.alertCtrl.create({
+      title: 'Connect',
+      message: 'Do you want to connect with?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Connect',
+          handler: () => {
+            this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
+        
+          }
+        }
+      ]
+    });
+    alert.present(
+      
+    );
 
   }
 
+
+  disconnect() {
+    let alert = this.alertCtrl.create({
+      title: 'Disconnect?',
+      message: 'Do you want to Disconnect?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            alert.dismiss();
+          }
+        },
+        {
+          text: 'Disconnect',
+          handler: () => {
+            this.bluetoothSerial.disconnect();
+            this.gettingDevices=null;
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+  next(){
+    this.navCtrl.push('TerminalPage');
+  }
+
+ data(){
+        setInterval(()=>{
+          this.read1();
+        } ,3000);
+
+        
+      }
+      
+
+read(){
+  this.bluetoothSerial.read().then((data)=>
+  {
+   this.message=data;
+    
+    })
+   
 }
+read1(){
+  this.ngZone.run(()=>{
+    this.read();
+  })
+}
+
+  
+  
+  }
